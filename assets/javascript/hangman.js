586070d7ec3382d10013c998e7bc4904e0cc38ac
mobiwahn <mpartee123>
2018-12-22 19:44:24 -0600

    second commit work in progress

--- assets/javascript/hangman.js
+++ assets/javascript/hangman.js
@@ -1,57 +1,72 @@
+// Get a random word
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 function incrementWinCounter() {
 
 }
 
-function guessLetter(letter) {
 
-}
+document.onkeyup = function (event) {
 
+    document.getElementById('guessed-letters').innerHTML = event.key;
+    return event.key;
+
+
+};
 /**
  *
  * @param {string} guessedWord
  */
 function drawGuessedWord(guessedWord) {
     document.getElementById('guessed-word').innerHTML = guessedWord;
-}
 
+}
 /**
  * Makes underscores given a word.
  * @param {string} word
  * @returns {string}
  */
 function generateUnderscores(word) {
-    var underscores = "";
 
+    var underscores = "";
     for (var i = 0; i < word.length; i++) {
         underscores += '_';
     }
     return underscores;
-}
 
+}
 /**
  * @returns {string}
  */
-function getRandomWord() {
-    var words = ['Ella Fitzgerald','Billie Holiday', 'Blah'];
+var randomWordResult = (function getRandomWord() {
+    var words = ['Ella Fitzgerald', 'Billie Holiday', 'Blah'];
     var randomIndex = Math.floor(Math.random() * words.length);
 
+    console.log(words[randomIndex]);
+
+
     return words[randomIndex];
+
+})();
+
+if (alphabet.indexOf(randomWordResult) === false) {
+    console.log("please type a LETTER");
 }
+
 
 /**
  * Initialize the app.
  */
-function init() {
-    // Get a random word
+function init(searchElement, fromIndex) {
     // Generate and show underscores for it, using the number of letters.
     // Show number of guesses remaining.
     // Show letters already guessed (empty at first)
+    drawGuessedWord(generateUnderscores(randomWordResult));
 
-    var randomWord = getRandomWord();
-    drawGuessedWord(generateUnderscores(randomWord));
+
 }
 
 // TODO: Run init when ready event fired.
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
     init();
 });
